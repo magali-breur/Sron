50bcfcdb3f467430e354b97d3e922c8e7f7c2839
magali-breur <magali.breur@outlook.com>
2025-01-09 12:18:59 +0100

    csv correct

--- london.py
+++ london.py
@@ -12,6 +12,8 @@ def airport_emission(location):
     date_density_list_2020 = []
     dates_2020 = []
 
+
+#2020 jaar
     # Lees het CSV-bestand
     with open(f'{location}_2020_1.csv', mode='r') as file:
         reader = csv.reader(file)
@@ -89,6 +91,166 @@ def airport_emission(location):
         x = x / 1000000000
         densities_calculated_2020.append(x)
     
+
+#2021 jaar
+# Lees het CSV-bestand
+    with open(f'{location}_2020_1.csv', mode='r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            
+            # Zorg ervoor dat je geen lege rijen hebt
+            if len(row) > 0:
+                date_and_density_2020 = row[0].strip()  # Verwijder eventuele extra spaties aan het begin of einde van de string
+                if '""' in date_and_density_2020:
+                    pass
+                else:
+                    # Als '""' niet in de string staat, splits dan op basis van de enkele dubbele aanhalingstekens
+                    parts_2020 = date_and_density_2020.split('"')
+                    if len(parts_2020) > 1:  # Zorg ervoor dat er een waarde na de datum is
+                        #print(f"Parts after alternative split: {parts}")
+                        date_2020 = parts_2020[0]
+                        density_2020 = parts_2020[1].strip()  # De waarde na de datum
+                        try:
+                            densities_2020.append(float(density_2020))  # Zet de density om naar een float
+                        except ValueError:
+                            print(f"Fout bij het converteren van density: {density_2020}")
+                        date_density_list_2020.append(parts_2020)
+                        
+                    if len(parts_2020) < 1:
+                        pass
+
+    print(date_density_list_2020)
+
+
+    dates_2020 = [item[0] for item in date_density_list_2020]  # Haal de datum uit elk element
+    densities_2020 = [float(item[1]) for item in date_density_list_2020 if item[1] != ""]  # Haal de density uit elk element en zet om naar float
+
+    # Print de twee lijsten
+    print("Dates:", dates_2020)
+    print("densities_2020:", densities_2020)
+
+
+
+    densities_2020_2  = []
+    date_density_list_2020_2 = []
+    dates_2020_2 = []
+    with open(f'{location}_2020_2.csv', mode='r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            
+            # Zorg ervoor dat je geen lege rijen hebt
+            if len(row) > 0:
+                date_and_density_2020_2 = row[0].strip()  # Verwijder eventuele extra spaties aan het begin of einde van de string
+                if '""' in date_and_density_2020_2:
+                    pass
+                else:
+                    # Als '""' niet in de string staat, splits dan op basis van de enkele dubbele aanhalingstekens
+                    parts_2020_2 = date_and_density_2020_2.split('"')
+                    if len(parts_2020_2) > 1:  # Zorg ervoor dat er een waarde na de datum is
+                        #print(f"Parts after alternative split: {parts}")
+                        date_2020_2 = parts_2020_2[0]
+                        density_2020_2 = parts_2020_2[1].strip()  # De waarde na de datum
+                        try:
+                            densities_2020_2.append(float(density_2020_2))  # Zet de density om naar een float
+                        except ValueError:
+                            print(f"Fout bij het converteren van density: {density_2020_2}")
+                        date_density_list_2020_2.append(parts_2020_2)
+                        
+                    if len(parts_2020_2) < 1:
+                        pass
+    dates_2020_2 = [item[0] for item in date_density_list_2020_2]  # Haal de datum uit elk element
+    densities_2020_2 = [float(item[1]) for item in date_density_list_2020_2 if item[1] != ""]  # Haal de density uit elk element en zet om naar float
+
+
+    dates_2020.extend(dates_2020_2)
+    densities_2020.extend(densities_2020_2)
+
+    densities_calculated_2020 = []
+    for x in densities_2020:
+        x = x / 1000000000
+        densities_calculated_2020.append(x)
+
+
+#2023 jaar
+# Lees het CSV-bestand
+    with open(f'{location}_2020_1.csv', mode='r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            
+            # Zorg ervoor dat je geen lege rijen hebt
+            if len(row) > 0:
+                date_and_density_2020 = row[0].strip()  # Verwijder eventuele extra spaties aan het begin of einde van de string
+                if '""' in date_and_density_2020:
+                    pass
+                else:
+                    # Als '""' niet in de string staat, splits dan op basis van de enkele dubbele aanhalingstekens
+                    parts_2020 = date_and_density_2020.split('"')
+                    if len(parts_2020) > 1:  # Zorg ervoor dat er een waarde na de datum is
+                        #print(f"Parts after alternative split: {parts}")
+                        date_2020 = parts_2020[0]
+                        density_2020 = parts_2020[1].strip()  # De waarde na de datum
+                        try:
+                            densities_2020.append(float(density_2020))  # Zet de density om naar een float
+                        except ValueError:
+                            print(f"Fout bij het converteren van density: {density_2020}")
+                        date_density_list_2020.append(parts_2020)
+                        
+                    if len(parts_2020) < 1:
+                        pass
+
+    print(date_density_list_2020)
+
+
+    dates_2020 = [item[0] for item in date_density_list_2020]  # Haal de datum uit elk element
+    densities_2020 = [float(item[1]) for item in date_density_list_2020 if item[1] != ""]  # Haal de density uit elk element en zet om naar float
+
+    # Print de twee lijsten
+    print("Dates:", dates_2020)
+    print("densities_2020:", densities_2020)
+
+
+
+    densities_2020_2  = []
+    date_density_list_2020_2 = []
+    dates_2020_2 = []
+    with open(f'{location}_2020_2.csv', mode='r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            
+            # Zorg ervoor dat je geen lege rijen hebt
+            if len(row) > 0:
+                date_and_density_2020_2 = row[0].strip()  # Verwijder eventuele extra spaties aan het begin of einde van de string
+                if '""' in date_and_density_2020_2:
+                    pass
+                else:
+                    # Als '""' niet in de string staat, splits dan op basis van de enkele dubbele aanhalingstekens
+                    parts_2020_2 = date_and_density_2020_2.split('"')
+                    if len(parts_2020_2) > 1:  # Zorg ervoor dat er een waarde na de datum is
+                        #print(f"Parts after alternative split: {parts}")
+                        date_2020_2 = parts_2020_2[0]
+                        density_2020_2 = parts_2020_2[1].strip()  # De waarde na de datum
+                        try:
+                            densities_2020_2.append(float(density_2020_2))  # Zet de density om naar een float
+                        except ValueError:
+                            print(f"Fout bij het converteren van density: {density_2020_2}")
+                        date_density_list_2020_2.append(parts_2020_2)
+                        
+                    if len(parts_2020_2) < 1:
+                        pass
+    dates_2020_2 = [item[0] for item in date_density_list_2020_2]  # Haal de datum uit elk element
+    densities_2020_2 = [float(item[1]) for item in date_density_list_2020_2 if item[1] != ""]  # Haal de density uit elk element en zet om naar float
+
+
+    dates_2020.extend(dates_2020_2)
+    densities_2020.extend(densities_2020_2)
+
+    densities_calculated_2020 = []
+    for x in densities_2020:
+        x = x / 1000000000
+        densities_calculated_2020.append(x)
+    
+
+#2024 jaar
     # Lijst om de density-waarden op te slaan
     densities_2024 = []
     date_density_list_2024 = []
@@ -132,8 +294,6 @@ def airport_emission(location):
     print("Dates:", dates_2024)
     print("densities_2024:", densities_2024)
 
-
-
     densities_2024_2  = []
     date_density_list_2024_2 = []
     dates_2024_2 = []
@@ -173,20 +333,30 @@ def airport_emission(location):
         x = x / 1000000000
         densities_calculated_2024.append(x)
 
+
+#dataframes voor alle jaren
     # # Maak pandas DataFrame's voor de jaren 2020 en 2024
     df_2020 = pd.DataFrame({'Date': dates_2020, 'Density': densities_calculated_2020})
+    df_2021 = pd.DataFrame({'Date': dates_2021, 'Density': densities_calculated_2021})
+    df_2023 = pd.DataFrame({'Date': dates_2023, 'Density': densities_calculated_2023})
     df_2024 = pd.DataFrame({'Date': dates_2024, 'Density': densities_calculated_2024})
 
     # # Zet de datumkolom als index
     df_2020['Date'] = pd.to_datetime(df_2020['Date'])
+    df_2021['Date'] = pd.to_datetime(df_2021['Date'])
+    df_2021['Date'] = pd.to_datetime(df_2023['Date'])
     df_2024['Date'] = pd.to_datetime(df_2024['Date'])
 
     #maanden aanmaken
     df_2020['month']= df_2020['Date'].dt.month
+    df_2021['month']= df_2021['Date'].dt.month
+    df_2023['month']= df_2023['Date'].dt.month
     df_2024['month']= df_2024['Date'].dt.month
 
     # # Groepeer per maand en bereken het maandgemiddelde
     df_2020.set_index('Date', inplace=True)
+    df_2021.set_index('Date', inplace=True)
+    df_2023.set_index('Date', inplace=True)
     df_2024.set_index('Date', inplace=True)
 
     #jaar dataframes aanmaken
